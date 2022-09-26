013288ee576e2f9202f7bd134230d9b4d6e025f5
Hiroyuki Adachi <hiroxpepe@gmail.com>
2022-09-26 21:11:33 +0900

    feat: #4 added game system improvements.

--- Assets/Scripts/Radar.cs
+++ Assets/Scripts/Radar.cs
@@ -14,6 +14,9 @@
  */
 
 using UnityEngine;
+using UnityEngine.UI;
+using UniRx;
+using UniRx.Triggers;
 
 namespace Studio.MeowToon {
     /// <summary>
@@ -23,20 +26,59 @@ namespace Studio.MeowToon {
     public class Radar : MonoBehaviour {
 
         ///////////////////////////////////////////////////////////////////////////////////////////////
+        // Constants
+
+        const int TARGETS_COUNT = 5;
+
+        ///////////////////////////////////////////////////////////////////////////////////////////////
         // References [bool => is+adjective, has+past participle, can+verb prototype, triad verb]
 
         [SerializeField]
-        GameObject player_object;
+        GameObject _player_object;
 
         [SerializeField]
-        GameObject direction_object;
+        GameObject _direction_object;
+
+        [SerializeField]
+        GameObject _targets_object;
+
+        [SerializeField]
+        GameObject _target_mark_object;
 
         ///////////////////////////////////////////////////////////////////////////
         // update Methods
 
+        // Awake is called when the script instance is being loaded.
+        void Awake() {
+        }
+
+        // Start is called before the first frame update.
+        void Start() {
+
+            // hide default mark.
+            _target_mark_object.GetComponent<Image>().enabled = false;
+
+            // get target positions
+            int idx = 1;
+            foreach (Transform target_transform in _targets_object.transform) {
+                var position = target_transform.position;
+                Debug.Log($"position.x: {position.x} position.y: {position.y} position.z: {position.z}");
+                var target_mark = Instantiate(_target_mark_object);
+                target_mark.name += $"_{idx}";
+                target_mark.transform.SetParent(transform, false);
+                target_mark.transform.localPosition = new Vector3(0, 0, 0);
+                target_mark.GetComponent<Image>().enabled = true;
+                idx++;
+            }
+
+            // Update is called once per frame.
+            this.UpdateAsObservable().Subscribe(_ => {
+            });
+        }
+
         void LateUpdate() {
-            Quaternion player_rotation = player_object.transform.rotation; // プレーヤーの y 軸を コンパスの z軸に設定する
-            direction_object.transform.rotation = Quaternion.Euler(0f, 0f, -player_rotation.eulerAngles.y);
+            Quaternion player_rotation = _player_object.transform.rotation; // set the player's y-axis to the radar direction's z-axis
+            _direction_object.transform.rotation = Quaternion.Euler(0f, 0f, -player_rotation.eulerAngles.y);
         }
     }
 }
